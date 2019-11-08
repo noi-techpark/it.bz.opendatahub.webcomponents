0bf5c38ceba74fd0ea08d1f2d1d4f2b789a58078
Peter Moser <p.moser@noi.bz.it>
2019-11-08 12:36:04 +0100

    Add production pipeline

---
+++ backend/Jenkinsfile-Prod.groovy
@@ -0,0 +1,82 @@
+pipeline {
+    agent {
+        dockerfile {
+            dir 'backend'
+            filename 'docker/dockerfile-java'
+            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
+        }
+    }
+
+    environment {
+        POSTGRES_URL = "jdbc:postgresql://postgres-prod.co90ybcr8iim.eu-west-1.rds.amazonaws.com:5432/webcompstore"
+        POSTGRES_USERNAME = credentials('webcompstore-prod-postgres-username')
+        POSTGRES_PASSWORD = credentials('webcompstore-prod-postgres-password')
+
+        WORKSPACE_PATH = "/var/data/webcomponents-store"
+        GITHUB_TOKEN = credentials('webcompstore-test-github-token')
+        ORIGIN_URL = "https://github.com/noi-techpark/odh-web-components-store-origins.git"
+        ORIGIN_BRANCH = "master"
+
+        DELIVERY_BASE_URL = "https://cdn.webcomponents.opendatahub.bz.it/dist"
+    }
+
+    stages {
+        stage('Configure') {
+            steps {
+                sh '''
+                    sed -i -e "s/<\\/settings>$//g\" ~/.m2/settings.xml
+                    echo "    <servers>" >> ~/.m2/settings.xml
+                    echo "        ${TESTSERVER_TOMCAT_CREDENTIALS}" >> ~/.m2/settings.xml
+                    echo "    </servers>" >> ~/.m2/settings.xml
+                    echo "</settings>" >> ~/.m2/settings.xml
+
+                    sed -i -e "s%\\(spring.datasource.url\\s*=\\).*\\$%\\1${POSTGRES_URL}%" backend/data-service/src/main/resources/application.properties
+                    sed -i -e "s%\\(spring.datasource.username\\s*=\\).*\\$%\\1${POSTGRES_USERNAME}%" backend/data-service/src/main/resources/application.properties
+                    sed -i -e "s%\\(spring.datasource.password\\s*=\\).*\\$%\\1${POSTGRES_PASSWORD}%" backend/data-service/src/main/resources/application.properties
+                    sed -i -e "s%\\(application.deliveryBaseUrl\\s*=\\).*\\$%\\1${DELIVERY_BASE_URL}%" backend/data-service/src/main/resources/application.properties
+                    sed -i -e "s%\\(application.workspace.path\\s*=\\).*\\$%\\1${WORKSPACE_PATH}%" backend/data-service/src/main/resources/application.properties
+                
+                    sed -i -e "s%\\(spring.datasource.url\\s*=\\).*\\$%\\1${POSTGRES_URL}%" backend/crawler-service/src/main/resources/application.properties
+                    sed -i -e "s%\\(spring.datasource.username\\s*=\\).*\\$%\\1${POSTGRES_USERNAME}%" backend/crawler-service/src/main/resources/application.properties
+                    sed -i -e "s%\\(spring.datasource.password\\s*=\\).*\\$%\\1${POSTGRES_PASSWORD}%" backend/crawler-service/src/main/resources/application.properties
+                    sed -i -e "s%\\(application.workspace.path\\s*=\\).*\\$%\\1${WORKSPACE_PATH}%" backend/crawler-service/src/main/resources/application.properties
+                    sed -i -e "s%\\(application.repository.github.token\\s*=\\).*\\$%\\1${GITHUB_TOKEN}%" backend/crawler-service/src/main/resources/application.properties
+                    sed -i -e "s%\\(application.repository.origin.url\\s*=\\).*\\$%\\1${ORIGIN_URL}%" backend/crawler-service/src/main/resources/application.properties
+                    sed -i -e "s%\\(application.repository.origin.branch\\s*=\\).*\\$%\\1${ORIGIN_BRANCH}%" backend/crawler-service/src/main/resources/application.properties
+
+                    sed -i -e "s%\\(spring.datasource.url\\s*=\\).*\\$%\\1${POSTGRES_URL}%" backend/delivery-service/src/main/resources/application.properties
+                    sed -i -e "s%\\(spring.datasource.username\\s*=\\).*\\$%\\1${POSTGRES_USERNAME}%" backend/delivery-service/src/main/resources/application.properties
+                    sed -i -e "s%\\(spring.datasource.password\\s*=\\).*\\$%\\1${POSTGRES_PASSWORD}%" backend/delivery-service/src/main/resources/application.properties
+                    sed -i -e "s%\\(application.workspace.path\\s*=\\).*\\$%\\1${WORKSPACE_PATH}%" backend/delivery-service/src/main/resources/application.properties
+                '''
+            }
+        }
+        stage('Install dependencies') {
+            steps {
+                sh 'cd backend && mvn install -B -U'
+            }
+        }
+        stage('Test/Build: data-service') {
+            steps {
+                sh 'cd backend/data-service && mvn -B -U clean test verify package'
+            }
+        }
+        stage('Test/Build: crawler-service') {
+            steps {
+                sh 'cd backend/crawler-service && mvn -B -U clean test verify package'
+            }
+        }
+        stage('Test/Build: delivery-service') {
+            steps {
+                sh 'cd backend/delivery-service && mvn -B -U clean test verify package'
+            }
+        }
+        stage('Archive') {
+            steps {
+                archiveArtifacts artifacts: 'backend/data-service/target/dataservice.war', onlyIfSuccessful: true
+                archiveArtifacts artifacts: 'backend/crawler-service/target/crawlerservice.war', onlyIfSuccessful: true
+                archiveArtifacts artifacts: 'backend/delivery-service/target/deliveryservice.war', onlyIfSuccessful: true
+            }
+        }
+    }
+}
