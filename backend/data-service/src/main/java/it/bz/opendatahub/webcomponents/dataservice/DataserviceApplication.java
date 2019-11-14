034c4ae79d93dc647f11df60e658441177532ef1
Peter Moser <p.moser@noi.bz.it>
2019-11-14 11:00:16 +0100

    Add EnableSwagger2 annotation

--- backend/data-service/src/main/java/it/bz/opendatahub/webcomponents/dataservice/DataserviceApplication.java
+++ backend/data-service/src/main/java/it/bz/opendatahub/webcomponents/dataservice/DataserviceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication
 @EntityScan({"it.bz.opendatahub.webcomponents.common.data.model", "it.bz.opendatahub.webcomponents.dataservice.data.model"})
 public class DataserviceApplication {
