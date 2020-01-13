ebb3040ee0c0a63845e3c8d63a262fb22921f927
Peter Moser <p.moser@noi.bz.it>
2020-01-13 16:31:07 +0100

    contact.vue: Improve entity data

--- frontend/src/pages/contact.vue
+++ frontend/src/pages/contact.vue
@@ -15,27 +15,44 @@
       <div>
         <h2>NOI Techpark</h2>
         <h3>Techtranfer - Digital Technologies</h3>
-        <p>
-          <strong>Phone :</strong> +39 0471 066 600 <br />
-          <strong>Web page :</strong>
-          <a class="text-secondary" href="https://noi.bz.it"
-            >https://noi.bz.it</a
-          >
-          <br />
-          <strong>email :</strong>
-          <a class="text-secondary" href="mailto:info@noi.bz.it"
-            >info@noi.bz.it</a
-          >
-          <br />
-          <strong>PEC :</strong>
-          <a class="text-secondary" href="mailto:noi@pec.noi.bz.it"
-            >noi@pec.noi.bz.it</a
-          >
-          <br />
-          <strong>Address :</strong> Volta str., 13/A, I-39100 Bolzano, Italy
-          <br />
-          <strong>VAT No. :</strong> IT 02595720216 <br />
-        </p>
+        <table class="entity">
+          <tr>
+            <td>Phone</td>
+            <td>+39 0471 066 600</td>
+          </tr>
+          <tr>
+            <td>Web Page</td>
+            <td>
+              <a class="text-secondary" href="https://noi.bz.it"
+                >https://noi.bz.it</a
+              >
+            </td>
+          </tr>
+          <tr>
+            <td>Email</td>
+            <td>
+              <a class="text-secondary" href="mailto:info@noi.bz.it"
+                >info@noi.bz.it</a
+              >
+            </td>
+          </tr>
+          <tr>
+            <td>PEC</td>
+            <td>
+              <a class="text-secondary" href="mailto:noi@pec.noi.bz.it"
+                >noi@pec.noi.bz.it</a
+              >
+            </td>
+          </tr>
+          <tr>
+            <td>Address</td>
+            <td>Volta str., 13/A, I-39100 Bolzano, Italy</td>
+          </tr>
+          <tr>
+            <td>VAT No.</td>
+            <td>IT 02595720216</td>
+          </tr>
+        </table>
       </div>
     </div>
   </div>
