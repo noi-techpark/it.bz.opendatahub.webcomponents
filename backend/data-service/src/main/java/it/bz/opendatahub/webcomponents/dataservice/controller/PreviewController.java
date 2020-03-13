cc0bd1d0eb34236580ead0b7e8d9583b483c7eab
Peter Moser <p.moser@noi.bz.it>
2020-03-13 16:07:05 +0100

    Remove unused imports

--- backend/data-service/src/main/java/it/bz/opendatahub/webcomponents/dataservice/controller/PreviewController.java
+++ backend/data-service/src/main/java/it/bz/opendatahub/webcomponents/dataservice/controller/PreviewController.java
@@ -10,12 +10,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import it.bz.opendatahub.webcomponents.common.data.rest.PreviewBody;
 import it.bz.opendatahub.webcomponents.common.data.rest.WebcomponentConfiguration;
 import it.bz.opendatahub.webcomponents.dataservice.service.WebcomponentService;
 import lombok.extern.slf4j.Slf4j;
