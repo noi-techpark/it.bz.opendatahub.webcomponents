8b016dfc876d3243d0351c0a89620df2bc0e343a
Peter Moser <p.moser@noi.bz.it>
2020-03-19 15:12:29 +0100

    data-service: Use query parameter instead of matrix variables
    This is, because Spring does not allow slashes (nor encoded slashes) in paths.
    The path pattern would not trigger for the current preview path if a matrix
    variable contains slashes in any form.

--- backend/data-service/src/main/java/it/bz/opendatahub/webcomponents/dataservice/controller/PreviewController.java
+++ backend/data-service/src/main/java/it/bz/opendatahub/webcomponents/dataservice/controller/PreviewController.java
@@ -3,6 +3,7 @@ package it.bz.opendatahub.webcomponents.dataservice.controller;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.StringJoiner;
 import java.util.Map.Entry;
@@ -35,7 +36,7 @@ public class PreviewController {
 	@GetMapping(value = "/{uuid}/{versionTag}")
 	public String getPreview(@PathVariable final String uuid, @PathVariable(required = true) final String versionTag,
 			@RequestParam(defaultValue = "") String style, @RequestParam(defaultValue = "") String slot,
-			@MatrixVariable Map<String, String> parameters, Model model) throws UnsupportedEncodingException {
+			@RequestParam(defaultValue = "") String attribs, Model model) throws UnsupportedEncodingException {
 
 		WebcomponentConfiguration conf;
 		if ("latest".equalsIgnoreCase(versionTag)) {
@@ -44,23 +45,59 @@ public class PreviewController {
 			conf = webcomponentService.getConfiguration(uuid, versionTag);
 		}
 
+		attribs = parseAttribs(attribs);
+
 		model.addAttribute("snippetScripts", conf.getScriptSources());
+
 		if (!style.isEmpty()) {
-			parameters.put("style", style);
+			attribs += " style=\"" + style + "\"";
 		}
 
-		StringJoiner sj = new StringJoiner(" ", " ", "");
-		for (Entry<String, String> entry : parameters.entrySet()) {
-			sj.add(entry.getKey() + "=\"" + decode(entry.getValue()) + "\"");
-		}
-
-		model.addAttribute("snippetCode", "<" + conf.getConfiguration().getTagName() + sj.toString() + ">"
-				+ decode(slot) + "</" + conf.getConfiguration().getTagName() + ">");
+		model.addAttribute("snippetCode", "<" + conf.getConfiguration().getTagName() + attribs + ">" + decode(slot)
+				+ "</" + conf.getConfiguration().getTagName() + ">");
 		return "preview";
 	}
 
 	private static String decode(final String rawString) throws UnsupportedEncodingException {
 		return URLDecoder.decode(rawString, StandardCharsets.UTF_8.name());
+	}
+
+	private static String parseAttribs(final String rawString) throws UnsupportedEncodingException {
+		boolean isKey = true;
+		boolean isValue = false;
+		String key = "";
+		String value = "";
+		String result = "";
+		for (int i = 0; i < rawString.length(); i++) {
+			char c = rawString.charAt(i);
+			if (isKey) {
+				switch (c) {
+					case '=':
+						isKey = false;
+						break;
+					case ';':
+					case ' ':
+						break;
+					default:
+						key += c;
+				}
+			} else if (isValue) {
+				switch (c) {
+					case '"':
+						result += " " + key.trim() + "=\"" + decode(value.trim()) + "\"";
+						isKey = true;
+						isValue = false;
+						key = "";
+						value = "";
+						break;
+					default:
+						value += c;
+				}
+			} else if (c == '"') {
+				isValue = true;
+			}
+		}
+		return result;
 	}
 
 }
