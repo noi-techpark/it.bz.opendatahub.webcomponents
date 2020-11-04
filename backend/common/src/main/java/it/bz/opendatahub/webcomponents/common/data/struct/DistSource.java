2ce492cb6f7be4887693c99fb77b33606e5b23d4
Peter Moser <p.moser@noi.bz.it>
2020-11-04 15:57:44 +0100

    common: Add missing class

---
+++ backend/common/src/main/java/it/bz/opendatahub/webcomponents/common/data/struct/DistSource.java
@@ -0,0 +1,13 @@
+package it.bz.opendatahub.webcomponents.common.data.struct;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DistSource {
+
+	private String file;
+	private String parameter;
+
+}
