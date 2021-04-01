033a43dda3158e55abd1f3ecbd943a958c63cd93
maxnocker <maximilian.nocker@konverto.eu>
2021-04-01 10:46:38 +0200

    feat: add publisher, contributer, partner section in footer

---
+++ frontend/src/components/quote-item.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="d-flex flex-column align-items-center">
+    <div class="circle-icon"></div>
+    <div class="text-uppercase font-weight-bold quote-title">
+      {{ title }}
+    </div>
+    <div class="quote-text">
+      {{ quote }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuoteItem',
+  props: {
+    title: { default: '', type: String },
+    quote: { default: '', type: String }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.quote-title {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.quote-text {
+  max-width: 240px;
+  margin-bottom: 10px;
+}
+</style>
--- frontend/src/layouts/partials/footer.vue
+++ frontend/src/layouts/partials/footer.vue
@@ -1,9 +1,43 @@
 <template>
   <footer>
+    <div class="p-5 text-center font-larger container">
+      <div class="text-uppercase h1">#Publishers #Contributors #Partners</div>
+      <hr style="width: 50px; background-color: white" />
+      <b-row class="quote-row">
+        <b-col cols="6" lg="3" md="6" sm="6" style="display: flex">
+          <quote-item
+            title="Contributing"
+            quote="Often the best way to get them is to put our hands into starting new
+            projects or contributing to existing ones."
+          ></quote-item>
+        </b-col>
+        <b-col cols="6" lg="3" md="6" sm="6" style="display: flex">
+          <quote-item
+            title="Sharing"
+            quote="There are several reasons you might want to contribute to open source software."
+          ></quote-item>
+        </b-col>
+        <b-col cols="6" lg="3" md="6" sm="6" style="display: flex">
+          <quote-item
+            title="Knowledge"
+            quote="It's useful regardless of whether or not you're a student."
+          ></quote-item>
+        </b-col>
+        <b-col cols="6" lg="3" md="6" sm="6" style="display: flex">
+          <quote-item
+            title="Networking"
+            quote="The fulfilling feeling of giving back and contributing to society is unparalleled."
+          ></quote-item>
+        </b-col>
+      </b-row>
+      <a href="https://noi.bz.it/en/privacy-cookie-policy" class="text-muted"
+        >learn more about or community</a
+      >
+    </div>
     <div class="get-in-touch">
-      <div class="container p-5 text-center font-larger">
+      <div class="p-5 text-center font-larger">
         <div class="text-uppercase h1 footer-text">get in touch</div>
-        <hr style="width: 10%; background-color: white" />
+        <hr style="width: 50px; background-color: white" />
         <div class="h1 footer-text">
           If you would like to become a member of the Open Data Hub Community as
           a Data Provider, Data Consumer, Web Component User or as a
@@ -58,5 +92,18 @@
 
 .footer-text {
   color: white;
+  margin-left: 10%;
+  margin-right: 10%;
+}
+
+.quote-row {
+  margin-top: 30px;
+  margin-bottom: 50px;
 }
 </style>
+<script>
+import QuoteItem from '~/components/quote-item'
+export default {
+  components: { QuoteItem }
+}
+</script>
