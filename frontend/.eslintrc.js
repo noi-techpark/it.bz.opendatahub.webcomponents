78e939c8ba4697df4f55d0dff522520d62bbf6ee
Andreas Hufler <andreas.hufler@konverto.eu>
2021-04-13 16:30:32 +0200

    style: fix prettier

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,18 +2,18 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'prettier',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
   ],
   plugins: ['prettier'],
   // add your custom rules here
   rules: {
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'off'
-  }
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
 };
