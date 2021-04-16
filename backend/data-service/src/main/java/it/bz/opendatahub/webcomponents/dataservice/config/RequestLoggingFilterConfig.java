b5751584a97bd39db1ed807c59613fab4f009394
Andreas Hufler <andreas.hufler@konverto.eu>
2021-04-16 14:08:31 +0200

    feat: add ability to log to console in json format

---
+++ backend/data-service/src/main/java/it/bz/opendatahub/webcomponents/dataservice/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,21 @@
+package it.bz.opendatahub.webcomponents.dataservice.config;
+
+import it.bz.opendatahub.webcomponents.common.logging.CustomRequestLoggingFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+	@Bean
+	public CustomRequestLoggingFilter logFilter() {
+		CustomRequestLoggingFilter filter
+			= new CustomRequestLoggingFilter();
+		filter.setIncludeQueryString(true);
+		filter.setIncludePayload(false);
+		filter.setIncludeHeaders(true);
+		filter.setIncludeClientInfo(true);
+		return filter;
+	}
+
+}
