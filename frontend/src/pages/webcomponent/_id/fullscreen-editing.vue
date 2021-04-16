7cb09ff4220ec821db984dac062c015363b480a8
maxnocker <maximilian.nocker@konverto.eu>
2021-04-16 16:49:27 +0200

    feat: add empty fullscreen editing page

---
+++ frontend/src/pages/webcomponent/_id/fullscreen-editing.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <detail-bottom-bar
+      :selected-view="selectedView"
+      @updatePreview="updatePreview"
+      @setSelectedView="setSelectedView"
+    >
+    </detail-bottom-bar>
+  </div>
+</template>
+
+<script>
+import DetailBottomBar from '~/components/detail-bottom-bar';
+export default {
+  name: 'FullscreenEditing.vue',
+  components: { DetailBottomBar },
+  layout(context) {
+    return 'fullscreen';
+  },
+  data() {
+    return {
+      selectedView: 'editing',
+    };
+  },
+  methods: {
+    setSelectedView(newSelectedView) {
+      this.selectedView = newSelectedView;
+    },
+    updatePreview() {},
+  },
+};
+</script>
