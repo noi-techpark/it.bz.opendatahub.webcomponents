2b020df94f9a67b8fd682d4570438bfb7161c5aa
Andreas Hufler <andreas.hufler@konverto.eu>
2021-04-27 16:13:03 +0200

    feat: add keycloak authentication

---
+++ backend/data-service/src/main/java/it/bz/opendatahub/webcomponents/dataservice/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package it.bz.opendatahub.webcomponents.dataservice.config;
+
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+	/**
+	 * Registers the KeycloakAuthenticationProvider with the authentication manager.
+	 */
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) {
+		auth.authenticationProvider(keycloakAuthenticationProvider());
+	}
+
+	@Bean
+	public KeycloakConfigResolver keycloakConfigResolver() {
+		return new KeycloakSpringBootConfigResolver();
+	}
+
+	/**
+	 * Defines the session authentication strategy.
+	 * For bearer-only applications there is no session needed and therefor
+	 * we use the NullAuthenticatedSessionStrategy.
+	 */
+	@Bean
+	@Override
+	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+		return new NullAuthenticatedSessionStrategy();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception
+	{
+		super.configure(http);
+		http.authorizeRequests()
+			.mvcMatchers(HttpMethod.OPTIONS).permitAll()
+			.mvcMatchers("/admin/**")
+			.authenticated()
+
+			.and()
+			.authorizeRequests()
+			.anyRequest()
+			.permitAll();
+	}
+}
