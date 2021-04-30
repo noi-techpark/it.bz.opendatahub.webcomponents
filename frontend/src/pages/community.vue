52453abb2e8f9568cc27cd3131b905b1619801de
maxnocker <maximilian.nocker@konverto.eu>
2021-04-30 16:08:42 +0200

    fix: replace community page with old content

--- frontend/src/pages/community.vue
+++ frontend/src/pages/community.vue
@@ -1,125 +1,42 @@
 <template>
-  <div>
-    <div class="bg-light">
-      <div class="container pt-4 pl-4 pr-4 pb-0 mb-5">
-        <h1>#Publishers #Contributors #Partners</h1>
-        <div>
-          Publishers have developed Web Components, that are published on the
-          Store.
-        </div>
-        <div>
-          Contributors have developed parts of the Open Data Hub Web Component
-          Store itself.
-        </div>
-        <div>
-          Partners worked together to establish the Open Data Hub Web Component
-          Store.
-        </div>
-        <div
-          class="row d-flex justify-content-start flex-column flex-md-row pt-5"
-        >
-          <div class="d-flex flex-row tab-buttons" style="overflow: scroll">
-            <div
-              :class="[
-                selectedTab === 'publishers'
-                  ? 'tab-button'
-                  : 'tab-button-disabled',
-              ]"
-              class="text-uppercase"
-              @click="selectTab('publishers')"
-            >
-              publishers
-            </div>
-            <div
-              :class="[
-                selectedTab === 'contributors'
-                  ? 'tab-button'
-                  : 'tab-button-disabled',
-              ]"
-              class="text-uppercase"
-              @click="selectTab('contributors')"
-            >
-              contributors
-            </div>
-            <div
-              :class="[
-                selectedTab === 'partners'
-                  ? 'tab-button'
-                  : 'tab-button-disabled',
-              ]"
-              class="text-uppercase"
-              @click="selectTab('partners')"
-            >
-              partners
-            </div>
-          </div>
-        </div>
-        <!--<p>
-          All companies, institutions and experts that are cooperating in this
-          project.
-        </p>
-        <p>
-          <a v-scroll-to="'#partners'" href="#" class="button">Partners</a>
-          <a v-scroll-to="'#publishers'" href="#" class="button">Publishers</a>
-          <a v-scroll-to="'#contributors'" href="#" class="button"
-            >Contributors</a
-          >
-        </p>
-        <h2 id="partners">Partners</h2>
-        <p>
-          Partners worked together to establish the Open Data Hub Web Component
-          Store.
-        </p>
-        <div class="logo_container">
-          <a class="img_container" href="https://noi.bz.it" target="_blank">
-            <img src="/NOI-logo.png" />
-          </a>
-          <a
-            class="img_container"
-            href="https://idm-suedtirol.com"
-            target="_blank"
-          >
-            <img src="/IDM-logo.png" />
-          </a>
-        </div>
-        <h2 id="publishers">Publishers</h2>
-        <p>
-          Publishers have developed Web Components, that are published on the
-          Store.
-        </p>
-        See Author tags inside
-        <router-link class="text-secondary" to="/">Dashboard</router-link>.
-        <h2 id="contributors">Contributors</h2>
-        <p>
-          Contributors have developed parts of the Open Data Hub Web Component
-          Store itself.
-        </p>
-        See
+  <div class="bg-light pb-5">
+    <div class="container p-4 font-larger">
+      <h1>Community</h1>
+      <p>
+        All companies, institutions and experts that are cooperating in this
+        project.
+      </p>
+      <p>
         <a
-          target="_blank"
-          class="text-secondary"
-          href="https://github.com/noi-techpark/odh-web-components-store/graphs/contributors"
-          >Contributors on GitHub</a
+          v-scroll-to="{ el: '#partners', offset: -100 }"
+          href="#"
+          class="button"
+          >Partners</a
         >
-        for further details.-->
-      </div>
-    </div>
-    <div v-if="selectedTab === 'publishers'" class="container">
-      See Author tags inside
-      <router-link class="text-secondary" to="/">Dashboard</router-link>
-    </div>
-    <div v-else-if="selectedTab === 'contributors'" class="container">
-      <contributor-item></contributor-item>
-      <contributor-item></contributor-item>
-      <contributor-item></contributor-item>
-      <contributor-item></contributor-item>
-      <contributor-item></contributor-item>
-      <contributor-item></contributor-item>
-    </div>
-    <div v-else-if="selectedTab === 'partners'" class="container">
+        <a
+          v-scroll-to="{ el: '#publishers', offset: -100 }"
+          href="#"
+          class="button"
+          >Publishers</a
+        >
+        <a
+          v-scroll-to="{ el: '#contributors', offset: -100 }"
+          href="#"
+          class="button"
+          >Contributors</a
+        >
+      </p>
+      <h2 id="partners">Partners</h2>
+      <p>
+        Partners worked together to establish the Open Data Hub Web Component
+        Store.
+      </p>
       <div class="logo_container">
         <a class="img_container" href="https://noi.bz.it" target="_blank">
           <img src="/NOI-logo.png" />
+        </a>
+        <a class="img_container" href="https://konverto.eu" target="_blank">
+          <img src="/KONVERTO_Logo.png" width="350" />
         </a>
         <a
           class="img_container"
@@ -129,23 +46,26 @@
           <img src="/IDM-logo.png" />
         </a>
       </div>
+      <h2 id="publishers">Publishers</h2>
+      <p>
+        Publishers have developed Web Components, that are published on the
+        Store.
+      </p>
+      See Author tags inside
+      <router-link class="text-secondary" to="/">Dashboard</router-link>.
+      <h2 id="contributors">Contributors</h2>
+      <p>
+        Contributors have developed parts of the Open Data Hub Web Component
+        Store itself.
+      </p>
+      See
+      <a
+        target="_blank"
+        class="text-secondary"
+        href="https://github.com/noi-techpark/odh-web-components-store/graphs/contributors"
+        >Contributors on GitHub</a
+      >
+      for further details.
     </div>
   </div>
 </template>
-<script>
-import ContributorItem from '@/components/contributor-item';
-
-export default {
-  components: { ContributorItem },
-  data() {
-    return {
-      selectedTab: 'publishers',
-    };
-  },
-  methods: {
-    selectTab(tabName) {
-      this.selectedTab = tabName;
-    },
-  },
-};
-</script>
