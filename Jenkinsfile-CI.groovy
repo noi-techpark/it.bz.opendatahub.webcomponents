7ddf6fc513cd692c80a11123b5f47a4756c08cae
Peter Moser <p.moser@noi.bz.it>
2021-06-15 18:22:16 +0200

    Update Jenkinsfile-CI.groovy

--- Jenkinsfile-CI.groovy
+++ Jenkinsfile-CI.groovy
@@ -6,7 +6,7 @@ pipeline {
             agent {
                 dockerfile {
                     dir 'backend'
-                    filename 'infrastructure/docker/dockerfile-java'
+                    filename 'infrastructure/docker/java.dockerfile'
                     additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
                 }
             }
