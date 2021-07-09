f1c66a08976bc004122072ecbb91ce4ab3f5104a
SirCotare <sircotare@3025-game.de>
2021-07-09 16:43:13 +0200

    chore: disable broken test coverage

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest',
   },
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: [
     '<rootDir>/src/components/**/*.vue',
     '<rootDir>/src/pages/**/*.vue',
