caa3f7229211cd862540b64469f10ed2b7d4ea93
SirCotare <sircotare@3025-game.de>
2021-07-09 16:20:32 +0200

    fix: allow framing of preview

--- backend/data-service/src/main/java/it/bz/opendatahub/webcomponents/dataservice/config/SecurityConfig.java
+++ backend/data-service/src/main/java/it/bz/opendatahub/webcomponents/dataservice/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception
 	{
 		super.configure(http);
-		http.csrf().disable().authorizeRequests()
+		http.csrf().disable().headers().frameOptions().disable().and().authorizeRequests()
 			.mvcMatchers(HttpMethod.OPTIONS).permitAll()
 			.mvcMatchers("/admin/**")
 			.authenticated()
