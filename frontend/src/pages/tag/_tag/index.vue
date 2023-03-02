42879f61ba06059e0103c6e889f23eb174e68888
Simon Dalvai <s.dalvai@noi.bz.it>
2023-03-02 16:54:03 +0100

    frontend: better /tag description

--- frontend/src/pages/tag/_tag/index.vue
+++ frontend/src/pages/tag/_tag/index.vue
@@ -1,10 +1,34 @@
 <template>
   <div id="tag-container">
-    <div class="container title">
+    <div class="container title-container">
       <div class="title pt-2">
-        <h1>Tag aliases</h1>
-        <p>Can't remember the long links of webcomponents?</p>
-        <p>Use the tag with /tag/webcomp-name</p>
+        <h1 v-if="!this.tagNotFound">Tag aliases</h1>
+        <h1 v-if="this.tagNotFound">404 tag not found</h1>
+        <p>
+          Can't remember the long links of webcomponents? Use the tag with
+          /tag/webcomp-name
+        </p>
+        <h2>Examples</h2>
+        <a href="/tag/carsharing">/tag/carsharing</a>
+        <br />
+        <a href="/tag/flight">/tag/flight</a>
+        <br />
+        <p>
+          You can also use the <strong>title</strong> of the webcomponent:<br />
+          <a href="/tag/Mobility E-Charging Map"
+            >/tag/Mobility E-Charging Map</a
+          >
+        </p>
+        <br />
+        <h2>Warning</h2>
+        <p>
+          Links might change and <strong>not work in the future</strong>, if for
+          example the title is used as a tag and the title gets changed.
+          <br />
+          Keep this in ind when sharing URLs with the tag and for critical
+          occasions please use the normal always valid URL you get when opening
+          a webcomponent with the uuid.
+        </p>
       </div>
     </div>
   </div>
@@ -13,18 +37,33 @@
 <script lang="ts">
 export default {
   name: 'Tag',
+  data() {
+    return {
+      tagNotFound: false,
+    };
+  },
   mounted() {
     const paths = this.$route.path.split('/');
     const tag = paths.pop();
-    console.log(tag);
-    this.$api.webcomponent.search(tag).then(function (result) {
-      console.log(result);
-      if (result.content && result.content.length > 0) {
-        const id = result.content[0].uuid;
-        console.log(id);
-        window.location.href = '../webcomponent/' + id;
-      }
-    });
+    this.getEvents(tag);
+  },
+  methods: {
+    async getEvents(tag) {
+      const notFound = await this.$api.webcomponent
+        .search(tag)
+        .then(function (result) {
+          console.log(result);
+          if (result.content && result.content.length > 0) {
+            const id = result.content[0].uuid;
+            console.log(id);
+            window.location.href = '../webcomponent/' + id;
+            return false;
+          } else {
+            return true;
+          }
+        });
+      this.tagNotFound = notFound;
+    },
   },
 };
 </script>
--- frontend/src/pages/tag/index.vue
+++ frontend/src/pages/tag/index.vue
@@ -1,14 +1,36 @@
 <template>
-  <div>
+  <div id="tag-container">
     <div class="container title-container">
       <div class="title pt-2">
         <h1>Tag aliases</h1>
-        <p>Can't remember the long links of webcomponents?</p>
-        <p>Use the tag with /tag/webcomp-name</p>
+        <p>
+          Can't remember the long links of webcomponents? Use the tag with
+          /tag/webcomp-name
+        </p>
+        <h2>Examples</h2>
+        <a href="/tag/carsharing">/tag/carsharing</a>
+        <br />
+        <a href="/tag/flight">/tag/flight</a>
+        <br />
+        <a href="/tag/flight">/tag/flight</a>
+        <br />
+        <a href="/tag/events">/tag/events</a>
+        <br />
+        <p>Note: You can also use the title</p>
+        <a href="/tag/Mobility E-Charging Map">/tag/Mobility E-Charging Map</a>
+
+        <br />
+        <h2>Warning</h2>
+        <p>
+          Links might change and not work in the future, if for example the
+          title is used and the title gets changed.
+        </p>
+        <p>
+          Keep this in ind when sharing URLs with the tag and for critical
+          occasions please use the normal always valid URL you et when opening a
+          webcomponent with the uuid.
+        </p>
       </div>
-    </div>
-    <div class="bg-light ml-auto">
-      <div class="container p-4 mb-5"></div>
     </div>
   </div>
 </template>
