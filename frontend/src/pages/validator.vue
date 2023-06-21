9810f0e04b4e3fd3092991f8c49d0f7156e13f50
henri <henri@moosbauer.com>
2023-06-21 17:10:55 +0200

    undo prev commit

--- frontend/src/pages/validator.vue
+++ frontend/src/pages/validator.vue
@@ -107,6 +107,8 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 
 <script>
 import WcsConfigTool from 'odh-web-components-configurator/src/components/wcs-configurator';
+import Ajv from 'ajv';
+import Schema from 'static/schemas/wcs-manifest-schema.json';
 import Example from 'static/wcs-manifest-example.json';
 import AceEditor from 'vue2-ace-editor';
 
@@ -115,19 +117,16 @@ export default {
   data() {
     return {
       wcsManifest: '',
+      Schema,
       Example,
       snipp: '',
       config: null,
       errors: null,
     };
   },
-  created() {
+  mounted() {
     this.wcsManifest = JSON.stringify(this.Example, null, 2);
     this.parseJson();
-  },
-  mounted() {
-    // this.wcsManifest = JSON.stringify(this.Example, null, 2);
-    // this.parseJson();
   },
   methods: {
     editorInit() {
@@ -138,22 +137,107 @@ export default {
     updateSnippet(data) {
       this.snipp = data;
     },
-    async parseJson() {
-      try {
-        const res = await fetch('/api/validator', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: this.wcsManifest,
-        });
+    getLineNumber(errorMsg) {
+      let lineNumber = null;
 
-        const validator = await res.json();
-
-        ({ config: this.config, errors: this.errors } = validator);
-      } catch (e) {
-        console.error(`Fetching manifest validation resulted in error: ${e}`);
+      /* Firefox gives us the line number */
+      let match = /line (\d+)/.exec(errorMsg);
+      if (match) {
+        lineNumber = match.length > 1 ? match[1] : match[0];
+      } else {
+        /* Other browsers just give a position in the JSON string */
+        match = /position (\d+)/.exec(errorMsg);
+        if (match) {
+          const position = match.length > 1 ? match[1] : match[0];
+          const tmp = this.wcsManifest.substring(0, position);
+          lineNumber = (tmp.match(/\n/g) || '').length + 1;
+        }
       }
+      return lineNumber;
+    },
+    parseJson() {
+      let wcsManifestParsed = {};
+      this.errors = [];
+      this.config = null;
+      const ajv = new Ajv({
+        $data: true,
+        verbose: false,
+        allErrors: true,
+        format: 'full',
+      });
+      ajv.addKeyword('validDefault', {
+        type: ['string', 'null', 'array'],
+        modifying: true,
+        validate(
+          schema,
+          data,
+          parentSchema,
+          dataPath,
+          parentData,
+          parentDataProperty,
+          rootData
+        ) {
+          return (
+            !!parentData.default &&
+            !!parentData.values &&
+            parentData.values.includes(parentData.default)
+          );
+        },
+        errors: true,
+      });
+      try {
+        const validate = ajv.compile(this.Schema);
+        wcsManifestParsed = JSON.parse(this.wcsManifest);
+        if (!validate(wcsManifestParsed)) {
+          const errors = validate.errors
+            .filter((error) => {
+              switch (error.message) {
+                case 'boolean schema is false':
+                case 'should match "then" schema':
+                case 'should match "else" schema':
+                case 'should match some schema in anyOf':
+                  return false;
+              }
+              return true;
+            })
+            .map((error) => {
+              if (
+                error.params.keyword &&
+                error.params.keyword === 'validDefault'
+              ) {
+                error.params.keyword = 'Choose any item inside .values';
+              }
+              return {
+                text: error.message,
+                path: error.dataPath,
+                params:
+                  Object.keys(error.params).length === 0 &&
+                  error.params.constructor === Object
+                    ? null
+                    : error.params,
+              };
+            });
+          this.errors = [...this.errors, ...errors];
+
+          return;
+        }
+      } catch (e) {
+        if (e instanceof SyntaxError) {
+          const lineNumber = this.getLineNumber(e.message);
+          this.errors = [
+            ...this.errors,
+            {
+              text:
+                'Syntax error' + (lineNumber ? ' @ line ' + lineNumber : ''),
+            },
+          ];
+        } else {
+          this.errors = [...this.errors, { text: e.message }];
+        }
+        return;
+      }
+      this.errors = null;
+      this.config = wcsManifestParsed.configuration;
     },
   },
 };
